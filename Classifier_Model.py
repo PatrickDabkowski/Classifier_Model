c56e2cb9be1571a65b0bb153e28fdd4e45fafa15
Patryk <84743742+PatrickDabkowski@users.noreply.github.com>
2022-07-22 01:27:46 +0200

    Convolutional Network

--- Classifier_Model.py
+++ Classifier_Model.py
@@ -1,6 +1,13 @@
 class Model():
-    def Bayes(self, X_train, y_train):
+    def __int__(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    def Bayes(self):
         from sklearn.naive_bayes import ComplementNB
         Bayesian = ComplementNB()
-        Bayesian.fit(X_train, y_train)
+        Bayesian.fit(self.X_train, self.y_train)
         return Bayesian
+    def CNN(self):
+        crit = int(self.X_train.shape[0] * 0.1)
+        self.X_train, self.X_valid = self.X_train[:-crit], self.X_train[-crit:]
+        self.y_train, self.y_valid = self.y_train[:-crit], self.y_train[-crit:]
