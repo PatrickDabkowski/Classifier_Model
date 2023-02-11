2d7c981a460c49f6f07651b118814c3021070b1a
Patryk <84743742+PatrickDabkowski@users.noreply.github.com>
2023-02-11 21:07:25 +0100

    with test

--- Test.py
+++ Test.py
@@ -1,5 +1,19 @@
 import Classifier_Model
+import Test_Images
 
-model = Classifier_Model.model
-
-
+def Test_is_a_one():
+    # given
+    test_img = Test_Images.Test_Image.is_one()
+    # when
+    model = Classifier_Model.model
+    pred = model.predict(test_img)
+    #then
+    assert pred == '1'
+def Test_is_not_one():
+    # given
+    test_img = Test_Images.Test_Image.is_not_one()
+    # when
+    model = Classifier_Model.model
+    pred = model.predict(test_img)
+    #then
+    assert pred == '0'
--- Test_Images.py
+++ Test_Images.py
@@ -8,7 +8,7 @@ class Test_Image:
         self.is_not_one = []
         self.augmented_one = []
         self.augmented_non_one = []
-    def is_one(self):
+    def is_one():
         #Image declaration
         img_one = np.zeros([28, 28])
         img_one[ 7:8, 12:16] = 90
@@ -16,9 +16,9 @@ class Test_Image:
         img_one[ 9:10, 8:10] = 50
         img_one[ 6:17, 14:16] = 110 - np.random.randint(30, size=(11, 2))
         # To make Image more realistic we performe erosion of dilation of image
-        self.img_one = skimage.morphology.erosion(skimage.morphology.dilation(img_one))
-        return self.img_one
-    def is_not_one(self):
+        img_one = skimage.morphology.erosion(skimage.morphology.dilation(img_one))
+        return img_one
+    def is_not_one():
         # Image declaration
         img_not_one = np.zeros([28, 28])
         img_not_one [ 8:18, 8:9] = 100 - np.random.randint(30, size=(10, 1))
@@ -26,8 +26,8 @@ class Test_Image:
         img_not_one [ 8:9, 8:19] = 100 - np.random.randint(30, size=(1, 11))
         img_not_one [ 18:19, 8:19] = 100 - np.random.randint(30, size=(1, 11))
         # To make Image more realistic we performe erosion of dilation of image
-        self.img_not_one = skimage.morphology.closing(img_not_one)
-        return self.img_not_one
+        img_not_one = skimage.morphology.closing(img_not_one)
+        return img_not_one
     def image_augmentation(self):
         #Augmentation - Colour Inversion
         self.augmented_one = util.invert(self.img_one)
